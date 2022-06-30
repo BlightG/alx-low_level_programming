04466cae55370bc30e7436ff4c7f814686b7db7f
BlightG <birhane.gulilat@gmail.com>
2022-06-30 19:41:41 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
+#include <stdio.h>
 #include <string.h>
-int puts(const char* ""Programming is like building a multilingual puzzle\n")
+
+int main() {
+	 
+	  // initializing strings
+	     char str1[] = "Programming is like building a multilingual puzzle";
+         // writing to stdout
+             puts(str1);
+             puts(str2);
+             return 0;
+           }
