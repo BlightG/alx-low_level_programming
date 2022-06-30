0719ad51bec3b8637de3a3eabcdcb0e7972c9fff
BlightG <birhane.gulilat@gmail.com>
2022-06-30 20:22:03 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  *  main - initalized main function
-/
+*/
 int main(void)
-{
+}
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	/* print the quoted text */
 	return (0);
