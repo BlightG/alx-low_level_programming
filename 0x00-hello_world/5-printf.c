257600f8d420720704dda76090a5b0ec779208c6
BlightG <birhane.gulilat@gmail.com>
2022-06-30 20:07:40 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <studio.h>
+int main(void)
+	/* initalized main function*/
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	/* print the quoted text */
+	return (0);
+}
