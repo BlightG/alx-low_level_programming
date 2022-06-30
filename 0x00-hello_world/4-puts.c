a2717e7ebd54f114056860edad7f0214d8e4d6d4
BlightG <birhane.gulilat@gmail.com>
2022-06-30 19:50:36 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-
-int main() {
-	 
-	  /* initializing strings */
-	     char str1[] = "Programming is like building a multilingual puzzle";
-         /* writing to stdout */
-             puts(str1);
-             puts(str2);
-             return 0;
-           }
+/**
+  * main - Entry point
+  *
+  * Description: A C program that prints with put function
+  *
+  * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	 puts("\"Programming is like building a multilingual puzzle');
+	 return 0;
+}
