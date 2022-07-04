68caaa91c56b5389524a85a57c33eefae61796eb
BlightG <birhane.gulilat@gmail.com>
2022-07-04 11:45:37 +0000

    task 11

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print the value of n status:
+ *              greater than, is zero and is not less than 6.
+ *
+ * Return: Always O (Success)
+*/
+int main(void)
+{
+int A, B, C, D;
+
+	for (A = 0; A <= 9; A++)
+	{
+		for (B = 0; B <= 9; B++)
+		{
+			for (C = 0; C <= 9; C++)
+			{
+				for (D = 0; D <= 9; D++)
+				{
+					putchar(A + 48);
+					putchar(B + 48);
+					putchar(' ');
+					putchar(C + 48);
+					putchar(D + 48);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
