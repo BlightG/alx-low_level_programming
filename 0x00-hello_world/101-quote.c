ac7e95f504656764412dd4563e731d2d9dfa595a
BlightG <birhane.gulilat@gmail.com>
2022-07-04 09:16:30 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,10 @@
  * Return: This time we return an error 1.
 */
 
-int main (void)
+int main(void)
 {
-	char ret [59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (1,ret,59);
+	char ret[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, ret, 59);
 	return (1);
 }
