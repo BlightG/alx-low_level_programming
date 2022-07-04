9bb8dd186c7a6c907f8b20dbc86d410dac39f899
BlightG <birhane.gulilat@gmail.com>
2022-07-04 09:23:24 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,8 @@
 
 int main(void)
 {
-	char ret[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char ret[59] =
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, ret, 59);
 	return (1);
