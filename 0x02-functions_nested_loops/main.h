50235fd57cc96120c01b1cddb84e59fa3681407e
BlightG <birhane.gulilat@gmail.com>
2022-07-05 14:11:58 +0000

    trying with hope

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,19 +7,12 @@ void print(void)
 {
 	printf("_putchar\n");
 }
-char _putchar(char c)
-{
-	char ret;
-
-	ret = putchar(c);
-	return ret;
-}
 void print_alphabet(void)
 {
 	int Alpha;
 	for (Alpha = 'a' ; Alpha <= 'z' ; Alpha++)
 	{
-		_putchar(Alpha);
+		putchar(Alpha);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
