9d80721d3f3c1e7f2657d19888e0cacde483075e
BlightG <birhane.gulilat@gmail.com>
2022-07-05 19:28:59 +0000

    first task 3 commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <ctype.h>
 /**
  * main - entry point
@@ -7,20 +6,8 @@
 */
 int _islower(int c)
 {
-	int c;
 	int ret;
-	
-	if (c <= 122 && c >= 97)
-	{
-	 	ret = 1;
-	}
-	else
-	{
-		ret = 0;
-	}
-	return ret;
-}
-int main (void)
-{
-	
+
+	ret = islower(c);
+	return ret;	
 }
