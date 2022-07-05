a3459ccb6c4306b4691a022db468f6462efce8a2
BlightG <birhane.gulilat@gmail.com>
2022-07-05 18:31:34 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * main - entry point
- * description: uses print alphabetfunction to print alphabet
- * return: 0
+ * description: uses print_alphabet function to print alphabet alphabet
+ * return: void 
 */
 void print_alphabet(void)
 {
-     int Alpha;
-     for (Alpha = 'a' ; Alpha <= 'z' ; Alpha++)
-     {
-     	_putchar(Alpha);
-     }
-     _putchar('\n');
+	int Alpha;
+
+	for (Alpha = 'a' ; Alpha <= 'z' ; Alpha++)
+	{
+		_putchar(Alpha);
+	}
+	_putchar('\n');
 }
