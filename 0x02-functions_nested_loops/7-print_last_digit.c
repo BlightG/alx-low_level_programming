85196c9e403f0cd6778b364cfa4755cdc8e9cfa2
BlightG <birhane.gulilat@gmail.com>
2022-07-06 11:49:38 +0000

    task 77

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,10 +7,10 @@
 int print_last_digit(int b)
 {
 	int lastDigit;
-	if (n < 0)
-		lastDigit = -1 * (n % 10);
+	if (b < 0)
+		lastDigit = -1 * (b % 10);
 	else
-		lastDigit = n % 10;
+		lastDigit = b % 10;
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
