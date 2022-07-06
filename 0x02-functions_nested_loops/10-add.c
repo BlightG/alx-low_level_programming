f472d9509ceee366b5dac4dae3a04ebab2a06a9f
BlightG <birhane.gulilat@gmail.com>
2022-07-06 08:40:15 +0000

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ * add - adds two inputs
+ * @A: first virable
+ * @B: second varible
+ * Return: returns addition
 */
 int add(int A, int B)
 {
 	int Ret;
-	
+
 	Ret = A + B;
 	return (Ret);
 }
