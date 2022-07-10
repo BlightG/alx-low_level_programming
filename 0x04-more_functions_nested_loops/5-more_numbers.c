463ab745fa5853911898a94f8edffc72d4a939c2
BlightG <birhane.gulilat@gmail.com>
2022-07-10 19:44:05 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints number from 1- 14 tem times
+ * Return: void
+*/
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0 ; a <= 10 ; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b + 48);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
