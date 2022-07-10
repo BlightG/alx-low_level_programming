f45e0f4cde3667d69de22c700b01889480e41881
BlightG <birhane.gulilat@gmail.com>
2022-07-10 22:00:13 +0000

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,14 @@ int main(void)
 
 	for (a = 0 ; a < 100 ; a++)
 	{
-		printf("%d", a);
 		if (a % 3 == 0)
 			printf("Fizz");
 		else if (a % 5 == 0)
 			printf("Buzz");
 		else if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz");
+		else
+			printf("%d", a);
 		putchar('\n');
 	}
 	return (0);
