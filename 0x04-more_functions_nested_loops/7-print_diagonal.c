f3b5cb1c1e341a3b53823f152ddc17392d2d44ce
BlightG <birhane.gulilat@gmail.com>
2022-07-10 21:26:03 +0000

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - prints an arrow
+ * @n: number
+*/
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+	for (a = 0 ; a <= n ; a++)
+	{
+		for (b = 0; b < a ; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar(47);
+		_putchar('\n');
+	}
+	}
+	else
+		_putchar('\n');
+}
