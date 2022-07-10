c5b70c45037b9544ce57e685cffe364fb36808b1
BlightG <birhane.gulilat@gmail.com>
2022-07-10 21:38:37 +0000

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - prints number from  - 14 ten times
+ * @size: variabel for size of square
+ * Return: a square with width and height of size
+*/
+
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0 ; a < size ; a++)
+	{
+		for (b = 0; b < size ; b++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
