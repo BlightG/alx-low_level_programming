493f1b3628c128057d80b1c13acc7ad902a04f45
BlightG <birhane.gulilat@gmail.com>
2022-07-11 18:39:57 +0000

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
- *
- *
- *
- *
-*/ 
+ * _strlen - prints size of a string
+ * @s: input, string.
+ * Return: size of string
+*/
 int _strlen(char *s)
 {
 	return (strlen(s));
