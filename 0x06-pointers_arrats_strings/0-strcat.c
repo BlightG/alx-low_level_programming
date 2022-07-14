aa459ffa455b51066b42dfd0968865927726be55
BlightG <birhane.gulilat@gmail.com>
2022-07-14 19:01:12 +0000

    first of many

---
+++ 0x06-pointers_arrats_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - appendes string src to the end of dest
+ * @dest: first  string
+ * @src: secondstring
+ * Return: the total string value
+*/
+char *_strcat(char *dest, char *src)
+{
+	int j, k, l, m;
+
+	m = 0;
+	for (j = 0 ; dest[j] != '\0' ; ++j)
+		;
+
+	for (k = 0 ; src[k]  != '\0' ; ++k)
+		;
+
+	for (l = j ; dest[l] == (j + k) ; l++)
+	{
+		dest[l] = src[m];
+		m++;
+	}
+	dest[l + 1] = '\0';
+	_putchar('\n');
+	return (dest);
+}
