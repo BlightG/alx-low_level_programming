b113c96f983be1751bbdd6f0376b48198cf379db
birhane <birhane.gulilat@gmail.com>
2022-07-26 22:41:44 +0300

    .\1-strdup.c v6

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- *
- *
+ * _strdup - function that crates space in memory for value of string
+ *@str: string to be compared
+ *Return: j or null
 */
 char *_strdup(char *str)
 {
@@ -9,21 +10,24 @@ char *_strdup(char *str)
 	char *j;
 
 	if (str == NULL)
-		return (NULL);
-	for(i = 0 ; str[i] != '\0' ; i++)
-	    ;
-	j = malloc((i + 1) * sizeof(char));
-	if (j == NULL)
-		return (NULL);
-	else 		
-	{
-        i = 0;
-        while (str[i] != '\0')
-        {
-            j[i] = str[i];
-            i++;            
-        }
-        return (j);
+    {
+        return (NULL);
     }
+    for (i = 0 ; str[i] != '\0' ; i++)
+	{
+        ;
+    }
+    j = malloc((i + 1) * sizeof(char));
+    if (j == NULL)
+	{
+        return (NULL);
+    }
+    i = 0;
+    while (str[i] != '\0')
+    {
+        j[i] = str[i];
+        i++;
+    }
+    return (j);
 }
 
