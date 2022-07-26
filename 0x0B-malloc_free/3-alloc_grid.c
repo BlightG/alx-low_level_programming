9754606536f7294e98794dbf772a2658bab22e97
birhane <birhane.gulilat@gmail.com>
2022-07-27 00:35:46 +0300

    .\3-alloc_grid.c v0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*
+*
+*/
+int **alloc_grid(int width, int height)
+{
+int i;
+char **j;
+
+if (width <= 0)
+    return (NULL);
+else if (height <= 0)
+    retrun (NULL);
+i = size of (int *) * width + size of (int) * width * height;
+j = malloc(len);
+if (j == NULL)
+    return (NULL);
+return (j);
+}
