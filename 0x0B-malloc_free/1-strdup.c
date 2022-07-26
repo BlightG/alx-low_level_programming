4286d1184fab038ac0982c0197b6c0ed6fb3dd4e
BlightG <birhane.gulilat@gmial.com>
2022-07-26 13:01:24 -0700

    1-strdup.c v8

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,28 +6,28 @@
 */
 char *_strdup(char *str)
 {
-    unsigned int i;
-    char *j;
+	unsigned int i;
+	char *j;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0 ; str[i] != '\0' ; i++)
-    {   
-        ;
-    }
-    j = malloc((i + 1) * sizeof(char));
-    if (j == NULL)
-    {
-        return (NULL);
-    }
-    i = 0;
-    while (str[i] != '\0')
-    {
-        j[i] = str[i];
-        i++;
-    }
-    return (j);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{   
+		;
+	}
+	j = malloc((i + 1) * sizeof(char));
+	if (j == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j[i] = str[i];
+		i++;
+	}
+	return (j);
 }
 
