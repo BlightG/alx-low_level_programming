217363d053e729255cb528de096e774cc2d9bf3f
birhane <birhane.gulilat@gmail.com>
2022-07-26 23:39:39 +0300

    .\2-str_concat.c v3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,13 +5,15 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-    unsigned int i, j;
+    unsigned int i, j, a, b;
     char *k;
 
     if (s1 == NULL)
         return ("");
     if (s2 == NULL)
         return ("");
+    if (s1 == NULL && s2 == NULL)
+        return (NULL);
     for (i = 0 ; s1[i] != '\0' ; i++)
         ;
     for (j = 0 ; s2[j] != '\0' ; j++)
@@ -20,7 +22,7 @@ char *str_concat(char *s1, char *s2)
     if (k == NULL)
         return (NULL);
     /*return variables ot zero and use them to conacte values of  s1 & s2*/
-    i = j = 0;
+    a = b = 0;
     while(s1[i] != '\0')
     {
         k[i] = s1[i];
