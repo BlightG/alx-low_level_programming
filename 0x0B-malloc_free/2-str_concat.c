800c8b817d791093a3911beb09a58208e12eae10
birhane <birhane.gulilat@gmail.com>
2022-07-26 23:51:50 +0300

    .\2-str_concat.c v6

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,9 +9,9 @@ char *str_concat(char *s1, char *s2)
     char *k;
 
     if (s1 == NULL)
-        s1 ("");
+        s1 = "";
     if (s2 == NULL)
-        s2 ("");
+        s2 = "";
     if (s1 == NULL && s2 == NULL)
         return (NULL);
     for (i = 0 ; s1[i] != '\0' ; i++)
