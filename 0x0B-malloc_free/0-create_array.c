9cfaa320b293b815b6cb7e27a57a3a912dac0038
BlightG <birhane.gulilat@gmial.com>
2022-07-25 19:04:31 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_array - creates an are of size szze with character c
+ *
+ * @size: unsigned int stating the size of size
+ * @c: char to fill the size of the matrix
+ *
+ * Return: null if size is 0 and ptr to first memory character if not
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	ptr = (char *) malloc(sizeof(char) * size);
+	for (i = 0 ; i < size; i++)
+	{
+		*(ptr + i) = c;
+	}
+	if (size == 0)
+		return (NULL);
+	else
+		return (ptr);
+}
