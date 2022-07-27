d634cadb44ab094279f189b0f91513450908aeec
birhane <birhane.gulilat@gmail.com>
2022-07-27 16:37:52 +0300

    .\101-strtow.c v7

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -34,7 +34,7 @@ for (i = 0 ; str[i] != '\0' ; i++)
             wordlength++;
         /*intializes colums for each row*/
         word[column] = (char *) malloc((wordlength + 1) * sizeof(char));
-        row = 0;
+        row = column = 0;
         /*prints each words on newly inzialized column*/
         for (wordprint = 0 ; wordprint < wordlength ; wordprint++)
         { 
