68c262ee367bb0cd32db49c3be6ff5db53868774
birhane <birhane.gulilat@gmail.com>
2022-07-27 10:35:32 +0300

    .\4-free_grid.c v0

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /*
+* free_grid - frees a 2d area with height of "height"
 *
-*
-*
+* @gird: 2d array
+* @height: height of array
 */
 void free_grid(int **grid, int height)
 {
 int i;
 
-for (i = 0 ; i <= height ; i++)
-{
+for (i = 0 ; i < height ; i++)
+    free (grid[i]);
 free (grid);
-grid++;
-}
 }
