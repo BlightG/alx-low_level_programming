21e9156933ea71b431a591d708db42f1973de72d
BlightG <birhane.gulilat@gmial.com>
2022-07-27 18:04:36 -0700

    argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,15 +5,12 @@
  * @argc: count of argumrnts
  * @argv: array of argument strings
  *
- * Returns: 0 if suceccsfull
+ * Return: 0 if suceccsfull
  *
 */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0 ; argv[i] != '\0' ; i++)
-		;
-	prtinf("%d\n", argc);
+	if (argv != NULL)
+		printf("%d\n", argc);
 	return (0);
 }
