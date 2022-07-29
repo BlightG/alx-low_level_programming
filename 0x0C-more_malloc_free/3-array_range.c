f1b3c870f917f7f21f0a32595490327f94c989d8
birhane <birhane.gulilat@gmail.com>
2022-07-29 09:29:46 +0300

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+int *print_range(int *ptr, int min, int max);
+{
+int i;
+
+for (i = min ; i <= max ; i++)
+{
+    ptr[i] = i;
+}
+    ptr[i] = '\0';
+    return (ptr);
+}
+/**
+*
+*
+*/
+int *array_range(int min, int max)
+{
+int *ptr;
+if (min > 0)
+    return (NULL);
+if (min > max)
+    return (NULL);
+ptr = malloc((max - min) + 2);
+if (ptr == NULL)
+    return (NULL);
+print_range(ptr, min, max);
+return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,5 @@
 #include <string.h>
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 #endif
