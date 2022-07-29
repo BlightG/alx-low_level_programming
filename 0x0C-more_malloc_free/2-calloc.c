ac5aeacb4ca3c38d158e95973e47c71c23d1bb7a
BlightG <birhane.gulilat@gmial.com>
2022-07-28 22:54:44 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,15 +6,12 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int i;
-void *ptr;
+	void *ptr;
 
-if (nmemb == 0 || size || == 0)
-    return (NULL);
-ptr = malloc(nmemb * sizeof(size));
-if (ptr == NULL)
-    return (NULL);
-for (i = 0 ; i < nmemb ; i++)
-    ptr[i] = 0;
-return (ptr);
+	if (nmemb == 0 || size == 0)
+	    return (NULL);
+	ptr = malloc(nmemb * sizeof(size));
+	if (ptr == NULL)
+	    return (NULL);
+	return (ptr);
 }
