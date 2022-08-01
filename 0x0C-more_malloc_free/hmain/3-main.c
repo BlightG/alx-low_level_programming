a6934444c720496c354eba7cb9dc68b3bb908028
BlightG <birhane.gulilat@gmial.com>
2022-07-31 17:19:36 -0700

    task

--- 0x0C-more_malloc_free/hmain/3-main.c
+++ 0x0C-more_malloc_free/hmain/3-main.c
@@ -40,8 +40,8 @@ int main(void)
 {
     int *a;
 
-    a = array_range(5, 4);
-    simple_print_buffer(a, 1);
+    a = array_range( 0, 11);
+    simple_print_buffer(a, 11);
     free(a);
     return (0);
 }
