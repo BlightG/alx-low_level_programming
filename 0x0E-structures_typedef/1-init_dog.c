13e3d8fc71612a2a81051e205c7eadc12738ecb4
BlightG <birhane.gulilat@gmial.com>
2022-08-01 05:34:41 -0700

    betty 1-init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,19 @@
 #include "dog.h"
 /**
+* init_dog - initalizes structure of dog
 *
-*
+* @d: pointer to struct
+* @name: first data point of structure
+* @age: second  "     "    "    "
+* @owner: third  "    "    "    "i
 *
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age; 
-	d->owner= owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
