0042288879a5ea62139d780080f03ea0dd28dc40
BlightG <birhane.gulilat@gmial.com>
2022-08-01 05:01:58 -0700

    dog betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - data for dog
+ * @name: dog name
+ * @age: age of dog
+ * @owner: dog owner name
+ *
+ * Description: longer description
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 #endif
