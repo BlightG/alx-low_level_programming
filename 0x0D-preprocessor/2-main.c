165b73087f70449952f373013c601d1cdca0b984
birhane <birhane.gulilat@gmail.com>
2022-08-02 16:09:48 +0300

    t

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+* main - entry point of function
+*
+* Descrtption: return source file name.
+*
+* Return: always returns 0 if sucess.
+*/
+int main(void)
+{
+    printf("%s\n", _FILE_);
+    return (0);
+}
