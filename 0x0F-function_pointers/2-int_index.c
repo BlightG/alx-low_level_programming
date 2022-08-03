eb2b3baa2dad1f4d8644c70cf9e305957e065424
birhane <birhane.gulilat@gmail.com>
2022-08-03 17:37:54 +0300

    1st

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,19 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			if (cmp(array[i]))
 			{
-				return (1);
-				break;
+				return (i);
 			}
 			else 
 			{
 				return (0);
-				break;
 			}
 		}
 	}
 	else if (size <= 0)
 		return (-1);
-	else 
-		return (-1);
+	return (-1);
 
 }
