745e753e01ca974dff955a99cbb64652c7d342d5
BlightG <birhane.gulilat@gmial.com>
2022-08-03 07:25:32 -0700

    t

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,3 +4,29 @@
 *
 *
 */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+	{
+		for(i = 0; array[i] != '\0' ; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (1);
+				break;
+			}
+			else 
+			{
+				return (0);
+				break;
+			}
+		}
+	}
+	else if (size <= 0)
+		return (-1);
+	else 
+		return (-1);
+
+}
