df25664c4de3890ab34d41c0246e8205c6aebd96
BlightG <birhane.gulilat@gmial.com>
2022-08-04 05:01:50 -0700

    varidadic sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,16 +8,18 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-    int i, j, sum;
-    sum = 0;
-    va_list args;
+	va_list args;
+	unsigned int i;
+	int j, sum;
 
-    va_start(args, n);
-    for (i = 0 ; i < n ; i++)
-    {
-        j = va_arg(args, int);
-        sum = sum + j;
-    }
-    va_ends(args);
-    retrun (args);
+	sum = 0;
+
+	va_start(args, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		j = va_arg(args, int);
+		sum = sum + j;
+	}
+	va_end(args);
+	return (j);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef VARIADIC
 #define VARIADIC
 #include <stdio.h>
-#include <strarg.h>
+#include <stdarg.h>
 #include <stdlib.h>
 int sum_them_all(const unsigned int n, ...);
-#endif VARIADIC
+#endif 
