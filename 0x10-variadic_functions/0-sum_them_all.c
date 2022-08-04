9ce095f226f52cec85b6f7aac0e259f18daf0bfc
BlightG <birhane.gulilat@gmial.com>
2022-08-04 05:43:02 -0700

    t

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,7 +10,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	int j, sum;
+	int sum;
 
 	if (n == 0)
 		return (0);
@@ -20,9 +20,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0 ; i < n ; i++)
 	{
-		j = va_arg(args, int);
-		sum = sum + j;
+		sum += va_arg(args, int);
 	}
 	va_end(args);
-	return (j);
+	return (sum);
 }
