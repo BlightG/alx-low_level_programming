6edfdcf61def672549f683b8a14567543da15fdb
BlightG <birhane.gulilat@gmial.com>
2022-08-04 08:37:08 -0700

    task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,48 @@
 #include "variadic_functions.h"
 /**
+* print_all - prints chracters with format specified in pointer
 *
-*
-*
+* @format: specifies the way to print
 */
 void print_all(const char * const format, ...)
 {
-    va_list arg;
-    int n, j , integer;
-    char character;
-    char* string;
-    float floating;
+	va_list arg;
+	int n, j, integer;
+	char character;
+	char *string;
+	float floating;
 
-    n = strlen(format);
-    va_start (arg, format);
-    j = 0;
-    while (j < n)
-    {
-        if (format[j] == 'i' || format[j] == 'c' || format[j] == 'f' ||format[j] == 's')
-        {
-            switch (format[j])
-            {
-                case 'i':
-                    integer = va_arg(arg,int);
-                    printf("%d", integer);
-                    break;
-                case 'c':
-                    character = va_arg(arg, int);
-                    printf("%c", character);
-                    break;
-                case 'f':
-                    floating = va_arg(arg, double);
-                    printf("%f", floating);
-                    break;
-                case 's': 
-                    string = va_arg(arg, char*);
-                    printf("%s", string);
-                    break;
-            }
-        if (j != (n - 1))
-            printf(", ");
-        }
-        j++;
-    }
-    printf("\n");
-    va_end(arg);
-
+	n = strlen(format);
+	va_start(arg, format);
+	j = 0;
+	while (j < n)
+	{
+		if (format[j] == 'i' || format[j] == 'c' || format[j] == 'f' || format[j] == 's')
+		{
+			switch (format[j])
+			{
+				case 'i':
+					integer = va_arg(arg, int);
+					printf("%d", integer);
+					break;
+				case 'c':
+					character = va_arg(arg, int);
+					printf("%c", character);
+					break;
+				case 'f':
+					floating = va_arg(arg, double);
+					printf("%f", floating);
+					break;
+				case 's':
+					string = va_arg(arg, char*);
+					printf("%s", string);
+					break;
+			}
+			if (j != (n - 1))
+				printf(", ");
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(arg);
 }
