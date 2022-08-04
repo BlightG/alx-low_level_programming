206b0b593a7f1f07188a0e09d3ab8567fc812acf
birhane <birhane.gulilat@gmail.com>
2022-08-04 18:05:52 +0300

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,5 +6,45 @@
 */
 void print_all(const char * const format, ...)
 {
-    
+    va_list arg;
+    const int n;
+    int j , integer;
+    char character;
+    char* string;
+    float floating;
+
+    n = strlen(format);
+    va_start (arg, format);
+    j = 0;
+    while (j < n)
+    {
+        if (format[j] == 'i', format[j] == 'c', format[j] == 'f', format[j] == 's')
+        {
+            switch (format[j])
+            {
+                case i:
+                    integer = va_arg(arg,int);
+                    printf("%d", integer);
+                    break;
+                case c:
+                    character = va_arg(arg, char);
+                    printf("%f", floating);
+                    break;
+                case f:
+                    floating = va_arg(arg, float)
+                    printf("%f", floating);
+                    break;
+                case s: 
+                    string = va_arg(arg, char*);
+                    printf("%s", string);
+                    break;
+            }
+        if (j != (n - 1))
+            printf(", ");
+        }
+        j++;
+    }
+    printf("\n");
+    va_end(arg);
+
 }
