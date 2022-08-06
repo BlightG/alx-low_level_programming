7efbadcf79b10e4e64612ac823986239dc09bb4b
birhane <birhane.gulilat@gmail.com>
2022-08-06 21:47:32 +0300

    t

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,35 @@ void print_number(int n)
 
     i = 1;
     k = 0;
-    do
+    if (n == 0)
+        _putchar(48);
+    else if ( n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
+    if (n > 0)
     {
         l = n / i;
-        i = i * 10;
-        k++;
-    }while(l);
-    for(j = 0; j < k ; j++)
-    {
-        i = i / 10;
-	    l = n / i;
-	    printf("%f\n", l);
+        while(l / 10 != 0)/* (402/10) (402/10) (40/10 ) */
+        {
+            l = n / i; /* (l = 402) (l = 40) (l = 4)*/
+            i = i * 10;/* (i = 10) (i = 100) (i = 1000)*/
+            k++; /* k = 1 k = 2 k = 3*/
+        }
+        for(j = 0; j < (k -1) ; j++)/* j = 0 j = 2*/
+        {
+            i = i / 10;/* (i = 100) (i = 10 ) */
+            l = (n / i) % (i / (i / 10)); /* (l = 4) (l = 40)*/
+           _putchar(l + 48);
+        }
+        if (j == (k - 1))
+        {
+            l = n % 10;
+            _putchar(l + 48);
+        }
+        if (n == 0)
+            _putchar(48);
     }
+
 }
