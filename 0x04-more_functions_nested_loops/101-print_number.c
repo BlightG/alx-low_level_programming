cc907c2da85f7f463fec94378cafa25c51f5ce87
BlightG <birhane.gulilat@gmial.com>
2022-08-06 08:42:23 -0700

    float

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
 *
 *
@@ -8,16 +8,19 @@ void print_number(int n)
     int i, j, k;
 
     i = 10;
-    k = 0
-    while(n % i)
-    {
-        i = i * 10;
-        k++;
-    }
-    for(j = 0; j <+ k ; k++)
-    {
-        i = i / 10;
-        _putchar(n / i);
+    k = 0;
+    if (n)
+    {	   
+	    while(n % i)
+	    {
+	        i = i * 10;
+	        k++;
+	    }
+   }
+   for(j = 0; j <= k ; k++)
+   {
+	i = i / 10;
+	_putchar(n / i);
     }
 _putchar('\n');
 }
