48e9ffa742f3de01f4ccb60397dd059f4e9b7e9a
birhane <birhane.gulilat@gmail.com>
2022-08-06 18:10:48 +0300

    4 late

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,22 +9,22 @@ void print_triangle(int size)
 {
     int i, j, printsize;
 
-        if (size <= 0)
-    {
+    if (size <= 0)
         _putchar('\n');
-        exit (1);
-    }
-    for (i = 1 ; i <= size ; i++)/*prints size amount of lines*/
+    else
     {
-        printsize = size - i;
-        for (j = 0 ; j < printsize; j++)
+        for (i = 1 ; i <= size ; i++)/*prints size amount of lines*/
         {
-            _putchar(' ');
+            printsize = size - i;
+            for (j = 0 ; j < printsize; j++)
+            {
+                _putchar(' ');
+            }
+            for (; printsize < size; printsize++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
         }
-        for (; printsize < size; printsize++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
     }
 }
