d42c9af930e2455f778e9048631895a96ba483f2
birhane <birhane.gulilat@gmail.com>
2022-08-06 18:50:38 +0300

    float

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,17 @@ void print_number(int n)
 {
     int i, j, k;
 
-    i = 10;
+    i = 1;
     k = 0;
-    if (n)
-    {	   
-	    while(n % i)
-	    {
-	        i = i * 10;
-	        k++;
-	    }
-        for(j = 0; j <= k ; k++)
-        {
-	        i = i / 10;
-	    _putchar( n / i);
-        }
+    while(n / i)
+    {
+        i = i * 10;
+        k++;
+    }
+    for(j = 0; j < k ; k++)
+    {
+        i = i / 10;
+    _putchar( n / i);
     }
     _putchar('\n');
 }
