cbb8cb62d4a8f29c4b0205d8b4dda978a98208aa
BlightG <birhane.gulilat@gmial.com>
2022-08-07 03:02:09 -0700

    fibinaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+* main - entry point to program
 *
-*
+* Return: 0 if sucess
 */
-int main (void)
+int main(void)
 {
-    long int i, fsum, fsumprev, fsumhold;
-    fsum = 1;
-    fsumprev = 0;
-    for (i = 1 ; i <= 50; i++)
-    {
-        fsumhold = fsum;/*fsh = 1, 1, 2, 3*/
-        fsum = fsum + fsumprev; /*fs = 1, 2, 3, 5,*/
-        fsumprev = fsumhold; /* fsp = 1, 1, 2*/
-        if (i != 50)
-            printf("%ld, ", fsum);
-        else
-            printf("%ld\n", fsum);
-    }
-    return(0);
+	long int i, fsum, fsumprev, fsumhold;
+
+	fsum = 1;
+	fsumprev = 0;
+	for (i = 1 ; i <= 50; i++)
+	{
+		fsumhold = fsum;/*fsh = 1, 1, 2, 3*/
+		fsum = fsum + fsumprev; /*fs = 1, 2, 3, 5,*/
+		fsumprev = fsumhold; /* fsp = 1, 1, 2*/
+		if (i != 50)
+			printf("%ld, ", fsum);
+		else
+			printf("%ld\n", fsum);
+	}
+	return (0);
 }
