3dad65279b6b6988c8b95308b8d8b4b087345847
BlightG <birhane.gulilat@gmial.com>
2022-08-09 12:05:30 -0700

    qe sera

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+* main - Entry point to a program
 *
-*
+* Return: Always Zero 0 if Sucess
 */
 int main (void)
 {
-    long unsigned int i, fsum, fsumprev, fsumhold;
-    fsum = 1;
-    fsumprev = 0;
-    for (i = 1 ; i <= 98; i++)
-    {
-        fsumhold = fsum;/*fsh = 1, 1, 2, 3*/
-        fsum = fsum + fsumprev; /*fs = 1, 2, 3, 5,*/
-        fsumprev = fsumhold; /* fsp = 1, 1, 2*/
-        if (i != 98)
-            printf("%lu, ", fsum);
-        else
-            printf("%lu\n", fsum);
-    }
-    return(0);
+	long unsigned int i, fsum, fsumprev, fsumhold;
+	
+	fsum = 1;
+	fsumprev = 0;
+	for (i = 1 ; i <= 98; i++)
+	{
+		fsumhold = fsum;/*fsh = 1, 1, 2, 3*/
+		fsum = fsum + fsumprev; /*fs = 1, 2, 3, 5,*/
+		fsumprev = fsumhold; /* fsp = 1, 1, 2*/
+		if (i != 98)
+			printf("%llu, ", fsum);
+		else
+			printf("%llu\n", fsum);
+	}
+	return(0);
 }
