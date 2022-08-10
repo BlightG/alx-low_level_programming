52ea385d8a03283e48e356976911d6d85f393c88
BlightG <birhane.gulilat@gmial.com>
2022-08-10 05:06:41 -0700

    5100atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,50 +1,53 @@
 #include "main.h"
 /**
-*
-*
+* revers - reverses an input integer.
+* @n: integr to be reversed
+* Return: retruns the reverse of n
 */
 int revers(int n);
 int revers(int n)
 {
-    int reverse, remainder;
-    reverse = 0;
-    while (n != 0)
-    {
-        remainder = n % 10;
-        reverse = reverse * 10 + remainder;
-        n /= 10;
-    }
-    return (reverse);
+	int reverse, remainder;
+
+	reverse = 0;
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reverse = reverse * 10 + remainder;
+		n /= 10;
+	}
+	return (reverse);
 }
 /**
+* _atoi - prints a number from a string of characters
 *
-*
+* @s: pointer to print numbers from
+* Return: int from s or 0
 */
 int _atoi(char *s)
 {
-    long int i , j, k, place, typcast;
-    
+	long int i, j, k, place, typcast;
 
-    k = 0;
-    for (i = 0 ; s[i] != '\0' ; i++)
-    {
-        j = 0;
-        place = 1;
-        while (s[i] >= 48 && s[i] <= 57)
-        {
-            typcast = s[i] - 48;
-            j =  j + typcast * place;
-            place = place * 10;
-            i++;
-            k++;
-        }
-        if (k)
-        {
-            if (s[i - k - 1] == '-')
-                j = -j;
-            j = revers(j);
-            return (j);
-        }
-    }
-    return (0);
+	k = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		j = 0;
+		place = 1;
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			typcast = s[i] - 48;
+			j =  j + typcast * place;
+			place = place * 10;
+			i++;
+			k++;
+		}
+		if (k)
+		{
+			if (s[i - k - 1] == '-')
+				j = -j;
+			j = revers(j);
+			return (j);
+		}
+	}
+	return (0);
 }
