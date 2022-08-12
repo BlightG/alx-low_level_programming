01641bec52ba4d5d64c05feb63379d991a2f16b1
birhane <birhane.gulilat@gmail.com>
2022-08-12 10:48:42 +0300

    reverse list order

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+*
+*
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+    /*check if **head is null;*/
+    list_t *newhead;
+
+    newhead = malloc(sizeof(list_t));
+    if (*head == NULL || head == NULL || newhead == NULL)
+        return (NULL);
+    /*duplicate value of str;*/
+    if (str != NULL)
+    {
+        newhead->str = strdup(str);
+        newhead->len = strlen(newhead->str);
+    }
+    newhead->next = *head;
+    head = &newhead;
+    return (head);
+}
