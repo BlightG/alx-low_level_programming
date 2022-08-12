41c89803e8c14fb991a92dd7edc3857fca9c001f
birhane <birhane.gulilat@gmail.com>
2022-08-12 22:42:26 +0300

    yte

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,53 @@
 #include "lists.h"
-/*
-*
-*
+
+/**
+ * add_node_end - adds a node at the end of the linked list
+ *
+ * @head: pointer to the first node of the list
+ * @str: string to add
+ *
+ * Return: NULL if it fails / starting addr of the list
 */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *newnode;
-    list_t *temppointer;
+	list_t *new_node, *temp;
+	size_t str_len = 0;
 
-    newnode = malloc(sizeof(list_t));
-    temppointer = malloc(sizeof(list_t));
-    /*printf("head:%p ,newnode:%p ,newnode->next:%p ,tempointer:%p , tempointer->next:%p", head, newnode, newnode->next, temppointer, temppointer->next);*/
-    if (newnode == NULL || temppointer == NULL || head == NULL)
-        return (NULL);
+	/*set strig length to 0 if its NULL*/
+	if (str == NULL)
+		str_len = 0;
 
-    if ( (*head)->next != NULL)
-    {
-        temppointer = (*head)->next;
-        while (temppointer->next != NULL)
-        {
-            temppointer = temppointer->next;
-            printf("temppointer:%p, temppointer ->next:%p ", temppointer,temppointer ->next);
-        }
-        temppointer->next = newnode;
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
 
-    /*printf("head:%p ,newnode:%p ,newnode->next:%p ,tempointer:%p , tempointer->next:%p\n", head, newnode, newnode->next, temppointer, temppointer->next);*/
-    }
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (str != NULL)
-    {
-        newnode->str = strdup(str);
-        newnode->len = strlen(newnode->str);
-        newnode->next = NULL;
-        printf("newnode->len:%d ,newnode->str:%s ", newnode->len, newnode->str);
-        /*printf("temppointer->len:%d , tempointer->str:%s", temppointer->len, temppointer->str);*/
-    }
-    /*free(temppointer);*/
-    return (temppointer->next);
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	new_node->next = NULL;
+
+	/*copy our starting node pointer to temp*/
+	temp = *head;
+
+	/*if temp is NULL place our new_node at the beginning of the list*/
+	/*else if its not null we place our new_node to temp->next hence*/
+	/*adding our new_node at the end of the list*/
+	if (temp == NULL)
+		*head = new_node;
+	else
+	{
+		/**
+		 * we iterate pointer next addr if its not NULL our temp addr
+		 * points to the next addr or the next node.
+		*/
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+	}
+
+	return (*head);
 }
