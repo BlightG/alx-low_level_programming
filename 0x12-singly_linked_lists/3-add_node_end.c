7a43c75ebfc04aecec2c209e1442a9fcc87fd5db
birhane <birhane.gulilat@gmail.com>
2022-08-12 19:32:25 +0300

    task 3 fist attempt

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/*
+*
+*
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+    list_t *newnode;
+    list_t *temppointer;
+
+    newnode = malloc(sizeof(list_t));
+    temppointer = malloc(sizeof(list_t));
+    /*printf("head:%p ,newnode:%p ,newnode->next:%p ,tempointer:%p , tempointer->next:%p", head, newnode, newnode->next, temppointer, temppointer->next);*/
+    if ((newnode || temppointer || head) == NULL)
+        return (NULL);
+
+    if ( (*head)->next != NULL)
+    {
+        temppointer = head;
+        while (temppointer->next != NULL)
+        {
+            temppointer = temppointer->next;
+            printf("temppointer:%p, temppointer ->next:%p ", temppointer,temppointer ->next);
+        }
+        temppointer->next = newnode;
+
+    /*printf("head:%p ,newnode:%p ,newnode->next:%p ,tempointer:%p , tempointer->next:%p\n", head, newnode, newnode->next, temppointer, temppointer->next);*/
+    }
+
+    if (str != NULL)
+    {
+        newnode->str = strdup(str);
+        newnode->len = strlen(newnode->str);
+        newnode->next = NULL;
+        printf("newnode->len:%d ,newnode->str:%s ", newnode->len, newnode->str);
+        /*printf("temppointer->len:%d , tempointer->str:%s", temppointer->len, temppointer->str);*/
+    }
+    /*free(temppointer);*/
+    return (temppointer->next);
+}
