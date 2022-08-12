aa468fb0031a43e20677be4fd419970e01f1725a
birhane <birhane.gulilat@gmail.com>
2022-08-12 11:56:17 +0300

    2-add_node.c is doing test main

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,15 +9,20 @@ list_t *add_node(list_t **head, const char *str)
     list_t *newhead;
 
     newhead = malloc(sizeof(list_t));
-    if (*head == NULL || head == NULL || newhead == NULL)
+    if (head == NULL || newhead == NULL)
         return (NULL);
+    /*printf("head:%p, newhead:%p, *head:%p, newhead->next: %p", head, newhead, *head, newhead->next);*/
     /*duplicate value of str;*/
     if (str != NULL)
     {
+        printf("str:%s, ", str);
         newhead->str = strdup(str);
+        /*printf("newhead->str:%s, ", newhead->str);*/
         newhead->len = strlen(newhead->str);
+        /*printf("newhead->len:%d\n ", newhead->len);*/
     }
     newhead->next = *head;
-    head = &newhead;
+    *head = newhead;
+    /*printf("head:%p, newhead:%p, *head:%p, newhead->next: %p\n", head, newhead, *head, newhead->next);*/
     return (*head);
 }
