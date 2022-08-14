9f3c2a0f0bf46a38b1f143c92788b56e527968ba
birhane <birhane.gulilat@gmail.com>
2022-08-14 03:38:47 +0300

    task 6 first attempt

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*
+*
+*/
+char *cap_string(char *str)
+{
+	int i, j;
+	char reference[12];
+
+	i = 0;
+	reference[12] = {' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		for (j = 0 ; reference[j] != '\0' ; j++)
+		{
+			if (str[i] == reference[j])
+			{
+				if(str[i + 1] >=  97 && str[i + 1] <= 122)
+				{
+					i++;
+					str[i] = str[i] - 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
+}
