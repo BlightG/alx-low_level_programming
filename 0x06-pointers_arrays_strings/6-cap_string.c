aa2a243eca2489357995ebf59868857bce0d5c27
birhane <birhane.gulilat@gmail.com>
2022-08-14 03:54:18 +0300

    task 6 second attampt done with switch case

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,74 @@
 #include "main.h"
 /**
+* cap_string - a function that capitalizes all the words
 *
+*@str: string to be capitalized
 *
+*Return: str or NULL
 */
 char *cap_string(char *str)
 {
-	int i, j;
-	char reference[12];
+	int i;
 
 	i = 0;
-	reference[12] = {' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
 	if (str == NULL)
 		return (NULL);
 	while (str[i] != '\0')
 	{
-		for (j = 0 ; reference[j] != '\0' ; j++)
+		switch (str[i])
 		{
-			if (str[i] == reference[j])
-			{
-				if(str[i + 1] >=  97 && str[i + 1] <= 122)
-				{
-					i++;
-					str[i] = str[i] - 32;
-				}
-			}
+			case(' '):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('\n'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case( ',' ):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case(';'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('.'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('!'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('?'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('"'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('('):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case(')'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('{'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			case('}'):
+				i++;
+				str[i] = str[i] - 32;
+				continue;
+			/*case():
+				str[i + 1] = str[i + 1] - 32;
+				break;*/
 		}
 		i++;
 	}
