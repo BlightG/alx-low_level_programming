3b1dea56df3e2dcbd8472ee46b0adc3d1be1bfc9
birhane <birhane.gulilat@gmail.com>
2022-08-14 03:09:07 +0300

    task 5 first trial

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*
+*
+*/
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if(str[i] >=  97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
