2d20a4b5a3897dd5c04ea9a0b4074064d9e2fb2c
birhane <birhane.gulilat@gmail.com>
2022-08-15 17:17:09 +0300

    task 1 first atempt

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+* listint_len - countshte length of the structure elements
+*
+* @h: linked list to be printed
+*
+* Return: number of nodes printed
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t nodecount;
+
+	nodecount = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodecount++;
+	}
+	return (nodecount);
+}
