deaa536cccf6532c2e7820e60216db76b66a2aff
birhane <birhane.gulilat@gmail.com>
2022-08-16 12:07:57 +0300

    task 4 demodification of task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -37,14 +37,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			temp = temp->next;
 		temp->next = new_node;
 	}
-	if (head == NULL)
-		free(head);
-	while ((*head)->next != NULL)
-	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-	}
 	free(new_node);
 	return (*head);
 }
