3541eef3d8f32a3bea3f36349895d96a11804a3e
BlightG <birhane.gulilat@gmial.com>
2022-08-16 12:19:41 -0700

    betty push for task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
-*
-*
+* free_listint2 - frees a sigly linked list
+* @head: head of a singly linked list
 */
 void free_listint2(listint_t **head)
 {
@@ -11,8 +11,9 @@ void free_listint2(listint_t **head)
 	while (curr != NULL)
 	{
 		listint_t *temp = curr;
+
 		curr = curr->next;
 		free(temp);
 	}
-	*head = NULL;
+	head = NULL;
 }
