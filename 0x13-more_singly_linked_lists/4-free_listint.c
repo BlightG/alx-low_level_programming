5acf33cedc084c7ff10aabe38ff48f34a9aae3db
birhane <birhane.gulilat@gmail.com>
2022-08-16 11:02:38 +0300

    task 4 first atempt

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+* free_listint - frees a singly linked list
+*
+* @head: head of linked list to be freed
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+
+	if (head == NULL)
+		free(head);
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	free(tmep);
+	free(head);
+
+
+
+}
