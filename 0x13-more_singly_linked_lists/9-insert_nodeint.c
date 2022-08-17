937dc03bf9e125348c756a5a80c5b867d3ce6c1f
birhane <birhane.gulilat@gmail.com>
2022-08-17 19:09:02 +0300

    task 9 first atempt

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,26 +1,49 @@
 #include "lists.h"
 /**
+* insert_nodeint_at_index - inserts a node at index idx
 *
+* @head: head of linked list
+* @idx: index of  new node
+* @n: node value
 *
+* Return: pointer tonew node or null if it failed
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int indexcount;
-	listint_t *temp;
+	listint_t *temp, *newnode, *shiftnode;
 
-	if (head == NULL)
+	/*check if head or newnode null*/
+	newnode = malloc(sizeof(listint_t));
+	shiftnode = malloc(sizeof(listint_t));
+	if (head == NULL || newnode == NULL || shiftnode == NULL || *head == NULL)
 		return (NULL);
 
-	temp = head;
+	/*initialize the values of temp and index-counter and n of newnode*/
+	temp = *head;
 	indexcount = 0;
-	while(indexcount < idx)
+	newnode->n = n;
+	/**
+	 * The while loop moves along the linked list by recusrsivly
+	 * equating values of temp to the next list value starting from head
+	 * and counting using indexcount until indexcount is one less that idx
+	 */
+	while (indexcount < idx)
 	{
 		if(temp != NULL)
 			temp = temp->next;
 		else
-			return(NULL);
+			return (NULL);
 		indexcount++;
 	}
-	temp->n = n;
-	return(temp);
+	/**
+	 * when the list reaches the appropriate node it will insert the newnode
+	 * by changing the pointer to newnode and using shiftnode to save value of the
+	 * succeeding pointer and then making the newnode's next pointer to point to shiftnode;
+	 *
+	 */
+	shiftnode = temp->next;
+	temp->next = newnode;
+	newnode->next = shiftnode;
+	return (newnode);
 }
