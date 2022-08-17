afa8f3962cdc57c398efba49ee75e1610f5a847c
BlightG <birhane.gulilat@gmial.com>
2022-08-17 09:59:21 -0700

    task 9 minor mod

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,10 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if(temp != NULL)
 			temp = temp->next;
 		else
-		{
-			printf("outside of range");
 			return (NULL);
-		}
 		indexcount++;
 	}
 	/**
