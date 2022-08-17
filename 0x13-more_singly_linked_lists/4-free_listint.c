fc44c0481fdecaca0278d8a3c609c01ce0f2aac6
birhane <birhane.gulilat@gmail.com>
2022-08-17 13:32:58 +0300

    task 4 repush

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+* free_listint - frees a singly linked list
+*
+* @head: head of linked list to be freed
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
