83b0afd3a8b752efaa456d5f8c5f35cfe666ed9e
birhane <birhane.gulilat@gmail.com>
2022-08-17 13:36:38 +0300

    task 5 adding if condintoong for head being null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,21 @@
 #include "lists.h"
 /**
-* free_listint2 - frees a sigly linked list
-* @head: head of a singly linked list
+*
+*
 */
 void free_listint2(listint_t **head)
 {
 	listint_t *curr;
 
-	curr = *head;
-	while (curr != NULL)
+	if (head != NULL)
 	{
-		listint_t *temp = curr;
-
-		curr = curr->next;
-		free(temp);
+		curr = *head;
+		while (curr != NULL)
+		{
+			listint_t *temp = curr;
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
