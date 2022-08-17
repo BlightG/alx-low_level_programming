0d013a5596468fb7e76fa8516e23a481103f8663
birhane <birhane.gulilat@gmail.com>
2022-08-17 19:55:34 +0300

    task 9 modified for case of idx =0 and head =null

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -38,6 +38,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new_node;
+		new_node->next = NULL;
 	}
 	return (*head);
 }
