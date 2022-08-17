cb29f6a17531d30e7d789bcb692e59c134f30624
BlightG <birhane.gulilat@gmial.com>
2022-08-17 03:29:21 -0700

    task 4 betty modification

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,14 +9,11 @@ void free_listint(listint_t *head)
 	listint_t *temp;
 
 
-	/*if (&head == NULL)
-		free(head);*/
 	while (head->next != NULL)
 	{
 		temp = head;
 		head = head->next;
 		free(temp);
 	}
-	free(temp);
 	free(head);
 }
