91ac417c49843162c62ca1ad7f0c9ba17427bae5
birhane <birhane.gulilat@gmail.com>
2022-08-18 22:49:51 +0300

    task 5 second atempt

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,16 +10,22 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i, flipsum;
+	size_t size;
 
 	/* flip sum is the count of the bits to flip*/
 	flipsum = 0;
+	size = 8 * sizeof(unsigned long int);
+	if (n & 1 << size)
+		n -= n;
+	if (m & 1 << size)
+		m -= m;
 	/**
 	* the for loop iterates from zero to the size of
 	* unsigned long int on the machine. for every bit it checks
 	* if it is 1 the checks if the corresponding digit on the
 	* other digit is 1 and same for 0.
 	*/
-	for (i = 0 ; i < (8 * sizeof(unsigned long int)) ; i++)
+	for (i = 0 ; i < size ; i++)
 	{
 		if (n & 1 << i)
 		{
