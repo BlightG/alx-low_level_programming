a2c65238f2fcc8a09c17a65fa36d22f6078cfbb0
birhane <birhane.gulilat@gmail.com>
2022-08-18 17:33:15 +0300

    task 0 first atempt

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+* binary_to_uint - changes binary to unsigned int
+*
+* @b: string of a binary number
+*
+* Return: an unisgned in value or o
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	int i, power;
+	unsigned int binary;
+	/* initializes values as zero*/
+	binary = power = 0;
+
+	/*check if pointer is null*/
+	if (b == NULL)
+		return (0);
+
+	/**
+	 * for lop used to count string length and
+	 * check for invalid characters
+	 */
+	for (i = 0 ; b[i] != '\0' ; i++)
+	{
+		if (b[i] == 49 || b[i] == 48)
+			;
+		else
+			return (0);
+	}
+	/**
+	 * uses i as value of string length starts from end
+	 * of string and goes to first value while adding
+	 * value of power from zero until string legth counter
+	 * i gets to zero
+	 */
+	for (i-- ; i >= 0 ; power++, i--)
+	{
+		binary = binary + ((b[i] - 48) * pow(2, power));
+	}
+	return (binary);
+}
