58c75a6018bca78b40e7aece33e173205bb157ba
BlightG <birhane.gulilat@gmial.com>
2022-08-22 09:26:33 -0700

    finished most

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,15 +3,14 @@
 * create_file - a function that creates a file
 *
 * @filename: pointer to a string for name of file
-* @textcontent: content to be filled inside the file
+* @text_content: content to be filled inside the file
 *
 * Return: 1 on success, -1 on failure
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file, readcheck;
-	ssize_t txtlength, wcount;
-	char *buffer;
+	int file, wcount;
+	ssize_t txtlength;
 
 	/*if the file name is NULL return -1*/
 	if (filename == NULL)
@@ -22,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	* read and write permissions to users
 	* if opening fails return -1
 	*/
-	file = open(filename, O_CREAT, O_RDWR, O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	/**
@@ -31,18 +30,8 @@ int create_file(const char *filename, char *text_content)
 	*/
 	for (txtlength = 0; text_content[txtlength] != '\0' ; txtlength++)
 		;
-	/*creates a buffer file with size txtlength*/
-	buffer = malloc(sizeof(char) * txtlength);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (-1);
-	}
-	readcheck = read(file, buffer,txtlength);
-	wcount = write(file,buffer,readcheck);
-	if (readcheck == -1)
-		return (-1);
+	wcount = write(file, text_content, txtlength);
 	if (wcount == -1)
-		return (-2);
+		return (-1);
 	return (1);
 }
