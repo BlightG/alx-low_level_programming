3468e65b50f22393317e2447750ea684dedc8e94
BlightG <birhane.gulilat@gmial.com>
2022-08-22 11:19:09 -0700

    2nd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,10 +7,11 @@
 *
 * Return: 1 on success, -1 on failure
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wcount;
+	int wcount;
 	ssize_t txtlength;
+	int file;
 
 	/*if the file name is NULL return -1*/
 	if (filename == NULL)
@@ -21,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	* read and write permissions to users
 	* if opening fails return -1
 	*/
-	file = open(filename, O_APPEND | O_RDWR | O_TRUNC, 0600);
+	file = open(filename, O_APPEND | O_RDWR | O_TRUNC, 0661);
 	if (file == -1)
 		return (-1);
 	/**
