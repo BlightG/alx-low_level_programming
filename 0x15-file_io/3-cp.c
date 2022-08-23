17062555d21593a6a50a45d979332d71a60f91ff
birhane <birhane.gulilat@gmail.com>
2022-08-23 13:23:22 +0300

    second atempt

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,48 +3,74 @@
 *
 *
 */
+#define MAXSIZE 1024
+
+
+/**
+ * __exit - prints error messages and exits with exit number
+ *
+ * @error: either the exit number or file descriptor
+ * @str: name of either file_in or file_out
+ * @fd: file descriptor
+ *
+ * Return: 0 on success
+*/
+int __exit(int error, char *str, int fd)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", str);
+			exit(error);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", str);
+			exit(error);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(error);
+		default:
+			return (0);
+	}
+}
+
 int main (int argc, char *argv[])
 {
 	ssize_t wcount, read_check, close_check;
 	int filefrom, fileto;
-	char *buffer;
+	char *buffer[MAXSIZE];
 
 	if (argc != 3)
-	{
-		dprintf(0,"Usage: cp file_from file_to\n");
-		exit(98);
-	}
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (1);
-	}
+		__exit(97, NULL, 0);
+
 	filefrom = open(argv[1], O_RDONLY);
-	read_check = read(filefrom, buffer, 1024);
-	if (read_check == -1 || filefrom == -1)
+	if (filefrom == -1)
+		__exit(98, argv[1], 0);
+
+	fileto = open(argv[2],O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fileto == -1)
+		__exit(99, argv[2], 0);
+
+	while ((read_check = read(filefrom, buffer, MAXSIZE)) != 0)
 	{
-		dprintf(0,"Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		if (read_check == -1)
+			__exit(98, argv[1], 0);
+
+		/*copy and write contents to file_out*/
+		wcount = write(fileto, buffer, read_check);
+		if (wcount == -1)
+			__exit(99, argv[2], 0);
 	}
-	fileto = open(argv[2],O_RDWR | O_TRUNC | O_CREAT, 0664);
-	wcount = write(fileto, buffer, read_check);
-	if (wcount == -1 || fileto == -1)
-	{
-		dprintf(0,"Error: Can't write to file %s\n", argv[2]);
-		exit (99);
-	}
-	close_check = close(filefrom);
+
+	close_check = close(filefrom); /*close file_in*/
 	if (close_check == -1)
-	{
-		dprintf(0,"Error: Can't close fd %d\n", filefrom);
-		exit (100);
-	}
-	close (fileto);
-		if (close_check == -1)
-	{
-		dprintf(0,"Error: Can't close fd %d\n", fileto);
-		exit (100);
-	}
+		__exit(100, NULL, filefrom);
+
+	close_check = close(fileto); /*close file_out*/
+	if (close_check == -1)
+		__exit(100, NULL, fileto);
+
 	return(0);
 }
