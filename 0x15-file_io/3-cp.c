84621cb48306d798394bbe329636e6bddd046546
birhane <birhane.gulilat@gmail.com>
2022-08-23 11:28:59 +0300

    first atempt

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+*
+*
+*/
+int main (int argc, char *argv[])
+{
+	ssize_t wcount, read_check, close_check;
+	int filefrom, fileto;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(0,"Usage: cp file_from file_to\n");
+		exit(98);
+	}
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer = NULL)
+	{
+		free(buffer);
+		return (1);
+	}
+	filefrom = open(argv[1], O_RDONLY);
+	if (filefrom == -1)
+	{
+		dprintf(0,"Error: Can't read from file %s", argv[1]);
+		exit(98);
+	}
+	read_check = read(filefrom, buffer, 1024);
+	if (read_check == -1)
+	{
+		dprintf(0,"Error: Can't read from file %s", argv[1]);
+		exit(98);
+	}
+	fileto = open(argv[2],O_RDWR | O_TRUNC | O_CREAT, 0664);
+	if (fileto == -1)
+	{
+		dprintf(0,"Error: Can't write to file %s", argv[2]);
+		exit(98);
+	}
+	wcount = write(fileto, buffer, 1024);
+	if (wcount == -1)
+	{
+		dprint(0,"Error: Can't write to file %s", argv[2]);
+		exit (99);
+	}
+	close_check = close(filefrom);
+	if (close_check == -1)
+	{
+		dprint(0,"Error: Can't close fd %d", filefrom);
+		exit (100);
+	}
+	close (fileto);
+		if (close_check == -1)
+	{
+		dprint(0,"Error: Can't close fd %d", fileto);
+		exit (100);
+	}
+	return(0;)
+}
