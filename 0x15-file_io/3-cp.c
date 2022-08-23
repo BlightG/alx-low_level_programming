c592bbb35f1f66124357c68babb6f62ac3620706
BlightG <birhane.gulilat@gmial.com>
2022-08-23 02:00:19 -0700

    mdodified error message

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,37 +23,37 @@ int main (int argc, char *argv[])
 	filefrom = open(argv[1], O_RDONLY);
 	if (filefrom == -1)
 	{
-		dprintf(0,"Error: Can't read from file %s", argv[1]);
+		dprintf(0,"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	read_check = read(filefrom, buffer, 1024);
 	if (read_check == -1)
 	{
-		dprintf(0,"Error: Can't read from file %s", argv[1]);
+		dprintf(0,"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fileto = open(argv[2],O_RDWR | O_TRUNC | O_CREAT, 0664);
 	if (fileto == -1)
 	{
-		dprintf(0,"Error: Can't write to file %s", argv[2]);
+		dprintf(0,"Error: Can't write to file %s\n", argv[2]);
 		exit(98);
 	}
 	wcount = write(fileto, buffer, read_check);
 	if (wcount == -1)
 	{
-		dprintf(0,"Error: Can't write to file %s", argv[2]);
+		dprintf(0,"Error: Can't write to file %s\n", argv[2]);
 		exit (99);
 	}
 	close_check = close(filefrom);
 	if (close_check == -1)
 	{
-		dprintf(0,"Error: Can't close fd %d", filefrom);
+		dprintf(0,"Error: Can't close fd %d\n", filefrom);
 		exit (100);
 	}
 	close (fileto);
 		if (close_check == -1)
 	{
-		dprintf(0,"Error: Can't close fd %d", fileto);
+		dprintf(0,"Error: Can't close fd %d\n", fileto);
 		exit (100);
 	}
 	return(0);
