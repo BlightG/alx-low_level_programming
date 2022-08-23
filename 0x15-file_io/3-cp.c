299125e9466ed1d7d23ffcd01beb707467f03df0
birhane <birhane.gulilat@gmail.com>
2022-08-23 12:33:18 +0300

    minor length cut

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,25 +21,15 @@ int main (int argc, char *argv[])
 		return (1);
 	}
 	filefrom = open(argv[1], O_RDONLY);
-	if (filefrom == -1)
-	{
-		dprintf(0,"Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	read_check = read(filefrom, buffer, 1024);
-	if (read_check == -1)
+	if (read_check == -1 || file from == -1)
 	{
 		dprintf(0,"Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fileto = open(argv[2],O_RDWR | O_TRUNC | O_CREAT, 0664);
-	if (fileto == -1)
-	{
-		dprintf(0,"Error: Can't write to file %s\n", argv[2]);
-		exit(98);
-	}
 	wcount = write(fileto, buffer, read_check);
-	if (wcount == -1)
+	if (wcount == -1 || fileto == -1)
 	{
 		dprintf(0,"Error: Can't write to file %s\n", argv[2]);
 		exit (99);
@@ -55,8 +45,6 @@ int main (int argc, char *argv[])
 	{
 		dprintf(0,"Error: Can't close fd %d\n", fileto);
 		exit (100);
-	
 	}
-	free(buffer);
 	return(0);
 }
