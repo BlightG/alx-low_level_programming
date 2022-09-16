987d2d704a4baeb9d1c5a5b442c136d137416d6b
BlightG <birhane.gulilat@gmail.com>
2022-09-16 00:46:34 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 __attribute__((__constructor__))
 void foo(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
