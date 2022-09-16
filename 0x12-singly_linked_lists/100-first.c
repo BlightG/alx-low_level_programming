b5623a87187996feeb0ecdda21a7134f44022ab5
BlightG <birhane.gulilat@gmail.com>
2022-09-16 00:44:33 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * foo - function that prints before main
+ *
+*/
+__attribute__((__constructor__))
+void foo(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	printf("I bore my house upon my back!\n");
+}
