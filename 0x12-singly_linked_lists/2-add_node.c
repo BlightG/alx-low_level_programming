e79961fcff17bcd752b171958705026fbe5f2f6b
BlightG <birhane.gulilat@gmail.com>
2022-09-16 00:16:37 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,25 +1,37 @@
 #include "lists.h"
 /**
+* add_node - a function that ads node at the end of list_t
 *
+* @head: a pointer to a pointer to sturcture list_t
+* @str: a str value to be added to structure list_t
 *
+* Return:: NULL or a pointer
 */
 list_t *add_node(list_t **head, const char *str)
 {
-    /*check if **head is null;*/
-    list_t *newhead;
+	list_t *newhead;
 
-    newhead = malloc(sizeof(list_t));
-    if (head == NULL || newhead == NULL)
-        return (NULL);
-    if (str != NULL)
-    {
-        newhead->str = strdup(str);
-        newhead->len = strlen(newhead->str);
-    }
-    if (*head == NULL)
+	newhead = malloc(sizeof(list_t));
+	if (head == NULL || newhead == NULL)
+	{
+	free(newhead);
+	return (NULL);
+	}
+	if (str != NULL)
+	{
+	newhead->str = strdup(str);
+		if (newhead->str == NULL)
+		{
+			free(newhead->str);
+			free(newhead);
+			return (NULL);
+		}
+		newhead->len = strlen(newhead->str);
+	}
+	if (*head == NULL)
 		newhead->next = NULL;
 	else
-        newhead->next = *head;
-    *head = newhead;
-    return (*head);
+		newhead->next = *head;
+	*head = newhead;
+	return (*head);
 }
