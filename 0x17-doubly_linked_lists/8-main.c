be4174c187b06e8216a9176973635126ed6f306c
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-29 19:31:00 +0300

    main

--- 0x17-doubly_linked_lists/8-main.c
+++ 0x17-doubly_linked_lists/8-main.c
@@ -10,7 +10,7 @@
  */
 int main(void)
 {
-	listint_t *head;
+	dlistint_t *head;
 
 	head = NULL;
 	add_dnodeint_end(&head, 0);
@@ -20,7 +20,7 @@ int main(void)
 	add_dnodeint_end(&head, 4);
 	add_dnodeint_end(&head, 98);
 	add_dnodeint_end(&head, 402);
-	dnodeint_end(&head, 1024);
+	add_dnodeint_end(&head, 1024);
 	print_dlistint(head);
 	printf("-----------------\n");
 	delete_dnodeint_at_index(&head, 5);
