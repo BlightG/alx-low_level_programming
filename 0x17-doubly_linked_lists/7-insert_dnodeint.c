8c95680b3f595954106fe17305e5230bb8001689
BlightG <birhane.gulilat@gmail.com>
2022-09-29 03:52:18 -0700

    second draft with full betty lint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,13 @@
 #include "lists.h"
 /**
+ * insert_dnodeint_at_index - a function that inserts a node at
+ *                            the nth of a doubly linked list
  *
+ * @h: a pointer to the linked list first pointer
+ * @idx: the index of the node to be added counted form 0
+ * @n:value of n at the idx node
  *
+ * Return: a pointer the new node or NULL
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -19,13 +25,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	newnode->n = n;
 	for (i = 0; i < idx ; i++)
 	{
-		if (temp != NULL) 
+		if (temp != NULL)
 			temp = temp->next;
 		else
+		{
+			free(newnode);
 			return (NULL);
+		}
 	}
 	newnode->next = temp;
 	newnode->prev = temp->prev;
+	if (idx)
+		newnode->prev->next = newnode;
+	else
+		*h = newnode;
 	temp->prev = newnode;
 	return (newnode);
 }
