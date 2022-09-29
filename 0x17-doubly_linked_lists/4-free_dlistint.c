fbee035f6afa5c3bca92dc31f1ac03dce9bea2fa
BlightG <birhane.gulilat@gmail.com>
2022-09-29 02:10:17 -0700

    secodn draft for 4 with betty fix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
+ * free_dlistint - frees a doubly linked list
  *
- *
+ * @head: pointer to head of struct
 */
 void free_dlistint(dlistint_t *head)
 {
@@ -10,10 +11,9 @@ void free_dlistint(dlistint_t *head)
 	temp = head;
 	if (temp == NULL || head == NULL)
 		exit(0);
-	while (temp != NULL)
+	while ((temp  = head) != NULL)
 	{
-		temp = temp->next;
-		free(temp->prev);
+		head = head->next;
+		free(temp);
 	}
-	free(temp);
 }
