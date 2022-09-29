428cba6dd2bf199fbfe66d7f668bb393d6508cd0
BlightG <birhane.gulilat@gmail.com>
2022-09-29 03:18:32 -0700

    made minor betty mode to first draft

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 /**
+ * sum_dlistint - a function that sums all
+ *                the data (n) of a dlistint_t linked list.
  *
+ * @head: head of a doubly linkked list
  *
+ * Return: sum of all n elements or 0
 */
 int sum_dlistint(dlistint_t *head)
 {
