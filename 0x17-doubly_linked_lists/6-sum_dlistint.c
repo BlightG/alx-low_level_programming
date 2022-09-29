c6ea44b5170fa02a055bf89df5125de321781fed
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-09-29 13:11:03 +0300

    first draft

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *
+ *
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	int sum;
+
+	if (head == NULL)
+		return (0);
+	temp = head;
+	sum = 0;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
