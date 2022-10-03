971a88077c52e89baaab7fecf06237dd5ebbc49a
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-03 17:00:55 +0300

    task 3 first atempt

---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,13 @@
+#ifndef OPERATIONS_H
+#define  OPERATIONS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+
+#endif /* OPERATIONS_H */
