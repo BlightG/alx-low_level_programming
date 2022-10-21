216bc87e5b15fac84869787b2f06fcde03a5cfd4
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 14:05:11 +0300

    betty mod

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,14 @@
 #include "hash_tables.h"
 /**
- * 
- * 
- * 
- * 
+ * key_index - a function to return the key for string @key
+ *             in an array of size @size
+ *
+ * @key: a string charachter that is going to be hash
+ * @size: size of the hash sructure
+ *
+ * Retrun: an index value where @key will be stored
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        return(hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
