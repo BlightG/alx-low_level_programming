ec3ac730326ee84ffc002ab8cce1214424b75fd8
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 18:08:28 +0300

    wed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,7 +6,7 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        unsigned long int index, i;
+        unsigned long int index;
         hash_node_t *hash_value, *temp;
 
         index = key_index((const unsigned char *)key, ht->size);
@@ -20,27 +20,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         hash_value->key = (char *) key;
         hash_value->value = (char *) value;
 
-        for (i = 0 ; i < index ; i++)
-                        ht->array++;
+        /*for (i = 0 ; i < index ; i++)
+                        ht->array++;*/
 
-        if(*(ht->array) == NULL)
+        if(ht->array[index] == NULL)
         {
-                /*printf("if\n");
-                printf("ht->array: %p", (void *) *ht->array);*/
+                /*printf("if\n");*/
                 hash_value->next = NULL;
-                *ht->array = hash_value;
+                ht->array[index] = hash_value;
+                printf("ht->array: %p, *ht->array->value: %s\n", (void *) ht->array[index], ht->array[index]->value);
         }
         else
         {       /*printf("else\n");*/
-                temp = (*ht->array);
+                temp = ht->array[index];
                 /*printf("temp->value: %s", temp->value);*/
                 while(temp->next != NULL)
                         temp = temp->next;
                 temp->next = hash_value;
         }
         /*printf("ht->array->value: %s\n", temp->value);*/
-        for (; i > 0 ; i--)
-                        ht->array--;
-        return (1);
+        /*for (; i > 0 ; i--)
+                        ht->array--;*/
+        printf("ht->array: %p, sizeof(ht->array): %d\n", (void *) ht->array[index], (int) sizeof(ht->array));
 
+        return (1);
 }
+
