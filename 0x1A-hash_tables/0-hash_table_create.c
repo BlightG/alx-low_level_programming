8fa8b1d9dba1d553585599621c36602c9496e9e5
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 12:12:36 +0300

    betty mod

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,17 +4,17 @@
  * 
  * @size: size of a hastable to create
  * 
- * Returns: a pointer to the newly created hash table
+ * Return: a pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    	hash_table_t *newhash;
+	hash_table_t *newhash;
 
 	newhash = malloc(sizeof(hash_table_t) * size);
-    	if (newhash == NULL)
-    	{
-	        free(newhash);
-        	return(NULL);
-    	}
-    	return (newhash);
+	if (newhash == NULL)
+	{
+		free(newhash);
+		return(NULL);
+	}
+	return (newhash);
 }
