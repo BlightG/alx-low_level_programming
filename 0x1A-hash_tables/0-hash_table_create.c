b020231f3bc622ae16060da40292279185d16167
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 11:14:16 +0300

    first draft for task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - a function to create a hash table
+ * 
+ * @size: size of a hastable to create
+ * 
+ * Returns: a pointer to the newly created hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    	hash_table_t *newhash;
+
+	newhash = malloc(sizeof(hash_table_t) * size);
+    	if (newhash == NULL)
+    	{
+	        free(newhash);
+        	return(NULL);
+    	}
+    	return (newhash);
+}
