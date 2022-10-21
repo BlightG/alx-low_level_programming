ff80bbb4052f49c5689ca372612062e72f9bd9df
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 15:52:56 +0300

    changed the size of newhash malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *newhash;
 	hash_node_t *ht;
 
-	newhash = malloc(sizeof(hash_table_t) * size);
+	newhash = malloc(sizeof(hash_table_t));
 	if (newhash == NULL)
 	{
 		free(newhash);
