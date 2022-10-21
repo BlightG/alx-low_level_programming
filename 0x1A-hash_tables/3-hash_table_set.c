37da21a946eec9f9bf6b8d1b30c0a90b8b7dfed3
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 16:23:05 +0300

    እስኪ ይሄን ሞክሩት

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,15 +9,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         unsigned long int index;
         hash_node_t *hash_value;
 
-        index = key_index(key, 1024);
-        
+        index = key_index((const unsigned char *)key, ht->size);
+        hash_value = malloc(sizeof(hash_node_t));
+        if (hash_value == NULL)
+        {
+                free(hash_value);
+                return (0);
+        }
 
         if(ht->array[index] == NULL)
         {
-                ht->array[index] = &hash_value;        
-                hash_value->key = key;
-                hash_value->value = value;
+                hash_value->key = (char *) key;
+                hash_value->value = (char *) value;
                 hash_value->next = NULL;
+                ht->array[index] = hash_value;        
         }
         return (1);
 }
