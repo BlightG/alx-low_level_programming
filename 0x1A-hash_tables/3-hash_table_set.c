74fa5874b146b220d2c0f9dae550805b6b409611
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 16:36:24 +0300

    asdf

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
                 hash_value->next = NULL;
                 ht->array[index] = hash_value;        
         }
-        printf("ht->array[index]->value: %s\n", ht->array[index]->value);
+        printf("index: %lu, \n", index);
         return (1);
 
 }
