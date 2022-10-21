accaad884acbbcd357a3b0649052af8b4ced8714
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-21 21:50:24 +0300

    buu

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,8 +6,9 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        unsigned long int index;
+        unsigned long int index, i;
         hash_node_t *hash_value, *temp;
+        hash_node_t **temp_array;
 
         index = key_index((const unsigned char *)key, ht->size);
         hash_value = malloc(sizeof(hash_node_t));
@@ -20,29 +21,28 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         hash_value->key = (char *) key;
         hash_value->value = (char *) value;
 
-        /*for (i = 0 ; i < index ; i++)
-                        ht->array++;*/
+        temp_array = ht->array;
+        for (i = 0 ; i < index ; i++)
+                        temp_array++;
 
-        if(ht->array[index] == NULL)
+        if(*temp_array == NULL)
         {
                 /*printf("if\n");*/
                 hash_value->next = NULL;
-                ht->array[index] = hash_value;
-                printf("ht->array: %p, *ht->array->value: %s\n", (void *) ht->array[index], ht->array[index]->value);
+                *temp_array = hash_value;
+                /*printf("ht->array: %p, *ht->array->value: %s\n", (void *) *temp_array, (*temp_array)->value);*/
         }
         else
-        {       /*printf("else\n");*/
-                temp = ht->array[index];
+        {
+                /*printf("else\n");*/
+                temp = *temp_array;
                 /*printf("temp->value: %s", temp->value);*/
                 while(temp->next != NULL)
                         temp = temp->next;
                 temp->next = hash_value;
         }
-        /*printf("ht->array->value: %s\n", temp->value);*/
-        /*for (; i > 0 ; i--)
-                        ht->array--;*/
-        printf("ht->array: %p, sizeof(ht->array): %d\n", (void *) ht->array[index], (int) sizeof(ht->array));
-
+        /*printf("ht->array->value: %s\n", temp->value);
+        printf("ht->array: %p, sizeof(ht->array): %d\n", (void *) *temp_array, (int) sizeof(temp_array));*/
         return (1);
 }
 
