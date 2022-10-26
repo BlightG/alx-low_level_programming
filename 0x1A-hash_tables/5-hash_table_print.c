6070018bac2f6a234229c75180b838731673b8a2
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-26 14:59:26 +0300

    T.5.6.1

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,23 +10,26 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *temp;
 	int n = 0;
 
-	if (ht != NULL || ht->array != NULL)
+	if (ht)
 	{
-		printf("{");
-		for (i = 0; i < ht->size; i++)
+		if (ht->array)
 		{
-			temp = ht->array[i];
-			while (ht->array[i] != NULL)
+			printf("{");
+			for (i = 0; i < ht->size; i++)
 			{
-				if (n == 1)
-					printf(", ");
-				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-				ht->array[i] = ht->array[i]->next;
-				n = 1;
-			}
-			ht->array[i] = temp;
+				temp = ht->array[i];
+				while (ht->array[i] != NULL)
+				{
+					if (n == 1)
+						printf(", ");
+					printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+					ht->array[i] = ht->array[i]->next;
+					n = 1;
+				}
+				ht->array[i] = temp;
 
+			}
+			printf("}\n");
 		}
-		printf("}\n");
 	}
 }
