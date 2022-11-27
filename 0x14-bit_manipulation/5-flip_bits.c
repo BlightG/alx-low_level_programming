01da52fb108faf678a1a958efb7ff811cbb434db
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-11-27 14:23:42 +0300

    bit manupilation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,7 +7,7 @@ float poww(double x, double y);
 */
 int *Unisgend_to_binary(unsigned long int xor, int* array)
 {
-	size_t i = sizeof(unsigned long int) * 8;
+	size_t i = sizeof(unsigned long int);
 	int j = 0;
 
 	if (xor == 0)
@@ -18,14 +18,12 @@ int *Unisgend_to_binary(unsigned long int xor, int* array)
 	printf("i = %i\n", (int) i);
 	while (i >= 1)
 	{
+		printf("pow(2 , i) = %f\n", poww((double) 2, (double) i));
 		if (xor >= poww(2, i) && xor < poww(2, (i + 1)))
 		{
-			/*printf("pow(2 , i) = %f\n", poww((double) 2, (double) i));*/
 			array[j] = 1;
 			xor = xor - poww(2 , i);
 		}
-		else
-			array[j] = 0;
 		i--;
 		j++;
 	}
@@ -78,13 +76,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xor = 0;
 	int *array;
 
-	array = malloc((sizeof(unsigned long int) * 8) + 1);
+	array = malloc(sizeof(unsigned long int) + 1);
 	if (!array)
 		return (0);
 	xor = n ^ m;
 
 	Unisgend_to_binary(m, array);
-	print_array(array, (sizeof(unsigned long int) * 8)+ 1);
+	print_array(array, sizeof(unsigned long int)+ 1);
 	free(array);
 	return (xor);
 }
