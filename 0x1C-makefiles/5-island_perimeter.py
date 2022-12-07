e44317c93558625957cc74a471305a8a73ecb9d8
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-07 23:37:10 +0300

    added guards and pycdodestyle passed;

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,13 +3,20 @@
 
 
 def island_perimeter(grid):
-    """ 
+    """
         a function that takes in a grid with height and width less than 100
         and returns the perimeter of the island that resides in it
     """
     peri = 0
+    if not isinstance(grid, list):
+        return None
+
     for i in range(len(grid)):
+        if not isinstance(grid[i], list):
+            return None
         for j in range(len(grid[i])):
+            if not isinstance(grid[i][j], list):
+                return None
             if i != 0 and grid[i][j] == 1:
                 peri += check_above(i, j, grid)
             if j != 0 and grid[i][j] == 1:
@@ -20,35 +27,38 @@ def island_perimeter(grid):
                 peri += check_below(i, j, grid)
 
     return peri
-                
+
 
 def check_above(i, j, grid):
     """ check for land or water above the current cell """
-    
+
     if grid[i - 1][j] == 1:
         return 0
     else:
         return 1
-            
+
+
 def check_left(i, j, grid):
     """ check for land or water left of the current cell """
-    
+
     if grid[i][j - 1] == 1:
         return 0
     else:
         return 1
-    
+
+
 def check_right(i, j, grid):
     """ check for land or water right of the current cell """
-    
+
     if grid[i][j + 1] == 1:
         return 0
     else:
         return 1
-    
+
+
 def check_below(i, j, grid):
     """ check for land or water below the current cell """
-    
+
     if grid[i + 1][j] == 1:
         return 0
     else:
