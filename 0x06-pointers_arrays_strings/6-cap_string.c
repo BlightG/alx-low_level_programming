3f366d8535da68be932c543a4aeb0bf13e5b2860
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-16 15:08:20 +0300

    modified first letter  parse  and tab parse

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,12 +26,20 @@ char *cap_string(char *str)
 	i = 0;
 	if (str == NULL)
 		return (NULL);
+
+	if (str[0] >=  97 && str[0] <= 122)
+		str[0] = str[0] - 32;
+
 	while (str[i] != '\0')
 	{
 		for (j = 0 ; reference[j] != '\0' ; j++)
 		{
 			if (str[i] == reference[j])
 			{
+				if (str[i] == 9)
+				{
+					str[i] = ' ';
+				}
 				if (str[i + 1] >=  97 && str[i + 1] <= 122)
 				{
 					i++;
