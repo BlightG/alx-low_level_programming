e7a28e71ee2fa7b7bc233297f6c9bf7dbe75d3a1
BlightG <birhane.gulilat@gmail.com>
2022-12-30 16:16:00 +0300

    betty for task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,25 +14,19 @@ int _strcmp(char *s1, char *s2)
 	s1i =  s2i = count = 0;
 	while ((s1[s1i] != '\0') && (s2[s2i] != '\0'))
 	{
-		/* printf("while 1, ");*/
-		count += s1[s1i] - s2[s2i]; 
-		/* printf("count = %d, s1[%d] = %c ,s2[%d] = %c \n",count, s1i, s1[s1i], s2i, s2[s2i]); */
+		count += s1[s1i] - s2[s2i];
 		s1i++;
 		s2i++;
 	}
 	while (s1[s1i] != '\0')
 	{
-		/* printf("while 2, "); */
-		count -= s1[s1i] - 106;		
+		count -= s1[s1i] - 106;
 		s1i++;
-		/* printf("count = %d, s1[%d] = %c ,s2[%d] = %c \n",count, s1i, s1[s1i], s2i, s2[s2i]); */
 	}
 	while (s2[s2i] != '\0')
 	{
-		/* printf("while 3, "); */
-		count += s2[s2i] - 106;		
+		count += s2[s2i] - 106;
 		s2i++;
-		/* printf("count = %d, s1[%d] = %c ,s2[%d] = %c \n",count, s1i, s1[s1i], s2i, s2[s2i]); */
 	}
 	return (count);
 }
