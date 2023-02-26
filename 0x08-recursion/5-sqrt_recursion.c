9ae861faec91b3e12796508c7d8be3acd4e0bba1
BlightG <birhane.gulilat@gmail.com>
2023-02-26 12:36:42 +0300

    betty commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,21 +7,21 @@
  * @max: maximum number to guess
  *
  * Return: square root of @n or -1
-*/
+ */
 int _sqrt_recursion_wrapper(int n, int min, int max)
 {
 	int guess, guess_squared;
 
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
+	guess = (min + max) / 2;       /*get guess (g)*/
+	guess_squared = guess * guess; /*square guess (sg)*/
 
-	if (guess_squared == n)			/*if squared guess == n return guess*/
+	if (guess_squared == n) /*if squared guess == n return guess*/
 		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
+	else if (min == max) /*this means n doesn't have a perfect square*/
 		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
+	else if (guess_squared < n) /*overshoot g if sg is less than the n*/
 		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
+	else /*undershoot g if sg is more than the n*/
 		return (_sqrt_recursion_wrapper(n, min, guess - 1));
 }
 
@@ -31,14 +31,14 @@ int _sqrt_recursion_wrapper(int n, int min, int max)
  *
  * @n: input number
  * Return: square root
-*/
+ */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
+	if (n == 1) /*sqrt(1) == 1*/
 		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
+	else if (n == 0) /*sqrt(0) == 0*/
 		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+	else if (n < 0) /*sqrt(-n) == -1 (NA)*/
 		return (-1);
 	else
 		return (_sqrt_recursion_wrapper(n, 1, n));
